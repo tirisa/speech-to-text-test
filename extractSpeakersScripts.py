0930582499e8b4b05068651480c82343ae30c9ac
taiga irisa <taiga_irisa@jp.honda>
2024-03-21 18:37:26 +0900

    extractSpeakersScriptsでvttファイルから話者の抽出ファイルを生成

--- extractSpeakersScripts.py
+++ extractSpeakersScripts.py
@@ -3,22 +3,26 @@ def extract_speaker_scripts(input_file, encoding='utf-8'):
         lines = file.readlines()
 
     speaker_scripts = {}
-    current_speaker = None
-    script = []
+    # current_speaker = None
+    # script = []
+    speakers = []
 
     for line in lines:
-        if line.startswith("<v"):
-            if current_speaker:
-                speaker_scripts[current_speaker] = script
-
-            current_speaker = line.split('<v ')[1].split('>')[0].strip()
-            script = []
-
-        elif line.startswith('</v>'):
-            continue
+        if line.startswith("<v"): 
+            speaker_name = line.split('<v ')[1].split('>')[0].strip()
+            script = line.split('>')[1].split('</v')[0].strip()
+            if speaker_name not in speakers:
+                speaker_scripts[speaker_name] = [script]
+                speakers.append(speaker_name)
+            else:
+                speaker_scripts[speaker_name].append(script)
 
         else:
-            script.append(line.strip())
+            continue 
+        # elif line.startswith('</v>'):
+        #     continue
+        # elif line.startswith('0'):
+        #     continue
 
     # Save each speaker's script to separate text files
     for speaker, script_lines in speaker_scripts.items():
